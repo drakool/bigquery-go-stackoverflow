1077736ca9213693965cbc647c7f5c69f3f554e8
Mohamed Hany <mohamed.hany@segmatek.com>
2024-01-05 16:24:31 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,49 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# 	https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Use the official Golang image to create a build artifact.
+# This is based on Debian and sets the GOPATH to /go.
+# https://hub.docker.com/_/golang
+
+FROM golang:1.19 AS build-stage
+
+# Create and change to the app directory.
+WORKDIR /app
+
+# Retrieve application dependencies.
+# This allows the container build to reuse cached dependencies.
+COPY go.* ./
+RUN go mod download
+
+# Copy local code to the container image.
+COPY .  ./
+
+RUN CGO_ENABLED=0 GOOS=linux go build -o /popular-go-articles
+
+# Run the tests in the container
+FROM build-stage AS run-test-stage
+RUN go test -v ./...
+
+# Deploy the application binary into a lean image
+FROM gcr.io/distroless/base-debian11 AS build-release-stage
+
+WORKDIR /
+
+COPY --from=build-stage /popular-go-articles /popular-go-articles
+
+EXPOSE 8080
+
+USER nonroot:nonroot
+
+ENTRYPOINT ["/popular-go-articles"]
---
+++ popular-go-articles.go
@@ -0,0 +1,115 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// [START bigquery_simple_app_all]
+
+// Command simpleapp queries the Stack Overflow public dataset in Google BigQuery.
+package main
+
+// [START bigquery_simple_app_deps]
+import (
+	"context"
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"cloud.google.com/go/bigquery"
+	"google.golang.org/api/iterator"
+)
+
+// [END bigquery_simple_app_deps]
+
+func main() {
+	projectID := os.Getenv("GOOGLE_CLOUD_PROJECT")
+	if projectID == "" {
+		fmt.Println("GOOGLE_CLOUD_PROJECT environment variable must be set.")
+		os.Exit(1)
+	}
+
+	// [START bigquery_simple_app_client]
+	ctx := context.Background()
+
+	client, err := bigquery.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatalf("bigquery.NewClient: %v", err)
+	}
+	defer client.Close()
+	// [END bigquery_simple_app_client]
+
+	rows, err := query(ctx, client)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := printResults(os.Stdout, rows); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// query returns a row iterator suitable for reading query results.
+func query(ctx context.Context, client *bigquery.Client) (*bigquery.RowIterator, error) {
+
+	// [START bigquery_simple_app_query]
+	query := client.Query(
+		`SELECT
+			title,
+			CONCAT(
+				'https://stackoverflow.com/questions/',
+				CAST(id as STRING)) as url,
+			view_count,
+			q.score, 
+			q.answer_count as answer_count,
+			q.creation_date creation_date
+		FROM ` + "`bigquery-public-data.stackoverflow.posts_questions` as q" + `
+		WHERE tags like '%go%' and 
+			q.comment_count > 10 and
+			q.creation_date between timestamp(DATE_SUB(current_date(), INTERVAL 2 year)) and timestamp(current_date())
+		ORDER BY view_count DESC
+		LIMIT 10;`)
+	return query.Read(ctx)
+	// [END bigquery_simple_app_query]
+}
+
+// [START bigquery_simple_app_print]
+type StackOverflowRow struct {
+	Title        string    `bigquery:"title"`
+	URL          string    `bigquery:"url"`
+	ViewCount    int64     `bigquery:"view_count"`
+	Score        int64     `bigquery:"score"`
+	AnswerCount  int64     `bigquery:"answer_count"`
+	CreationDate time.Time `bigquery:"creation_date"`
+}
+
+// printResults prints results from a query to the Stack Overflow public dataset.
+func printResults(w io.Writer, iter *bigquery.RowIterator) error {
+	for {
+		var row StackOverflowRow
+		err := iter.Next(&row)
+		if err == iterator.Done {
+			return nil
+		}
+		if err != nil {
+			return fmt.Errorf("error iterating through results: %w", err)
+		}
+		fmt.Fprintf(w, "title: %s createion-date: %v url: %s views: %d\n", row.Title, row.CreationDate, row.URL, row.ViewCount)
+		//fmt.Fprintf(w, "title: %s createion-date: %v url: %s views: %d\n", row.Title, row.CreationDate, row.URL, row.ViewCount)
+	}
+}
+
+// [END bigquery_simple_app_print]
+// [END bigquery_simple_app_all]
